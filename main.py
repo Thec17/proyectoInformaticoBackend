a8e6fe47f16d4c4cd8272f05f2b762fd64e72311
Thec17 <137742154+Thec17@users.noreply.github.com>
2026-05-10 15:13:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ app = FastAPI(title="AWB Backend API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://proyectoinformaticoenriquemolina.netlify.app/"],
+    allow_origins=["https://proyectoinformaticoenriquemolina.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
